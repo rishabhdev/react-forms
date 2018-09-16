1d5b97bb2a80d0608b273c5cb7de9197fe73bdc3
rishabhdev <rishabh.dev@fitkids.co.in>
2018-09-16 08:00:07 +0530

    deleted unnecessary code

--- src/components/todoInput/todoInput.js
+++
@@ -1,8 +0,0 @@
-class TodoInput extends React.Component{
-    render(){
-        return (<div className="todo-input-wrapper">
-                    <div className="todo-input"><input type="text" /></div>
-                </div>);
-    }
-}
-window.TodoInput = TodoInput;
